3c75447297b73ad2a0c9657af25f695d3a0311a6
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2024-01-19 23:54:16 +0100

    try to connect postgres to back at host

--- src/db/ormconfig.ts
+++ src/db/ormconfig.ts
@@ -4,7 +4,7 @@ import { Book } from 'src/book/book.entity';
 
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'dpg-cmi4a8i1hbls738dgsu0-a.frankfurt-postgres.render.com',
+  host: 'dpg-cmi4a8i1hbls738dgsu0-a',
   port: 5432,
   username: process.env.DB_USERNAME || 'bookme_api_user',
   password: process.env.DB_PASSWORD || 'SjsqYSORXEekm2D9sKf8d2moibqdymUH',
