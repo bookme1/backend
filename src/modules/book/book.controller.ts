6456fd67bc30990eae9a6e424de6a90a68c73bec
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2024-07-12 18:35:31 +0200

    fix limit books

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -18,7 +18,6 @@ import {
   deliverDTO,
   EditBookDto,
   FilterBookDto,
-  FindBookDto,
   SaveBookDto,
   WatermarkDTO,
 } from './book.dto';
--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -386,7 +386,7 @@ export class BooksService {
       }
 
       const filteredBooks = await queryBuilder.getMany();
-      return filteredBooks.slice(0, 20);
+      return filteredBooks;
     } catch (error) {
       throw new Error(`Error filtering books: ${error.message}`);
     }
