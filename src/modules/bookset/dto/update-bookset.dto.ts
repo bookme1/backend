32a6c85d6141e1023e2699e46d6d4a687e4a1b3a
Viacheslav <19wiltor69@gmail.com>
2024-10-02 21:27:29 +0300

    create bookset

---
+++ src/modules/bookset/dto/update-bookset.dto.ts
@@ -0,0 +1,20 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateBooksetDto } from './create-bookset.dto';
+import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class UpdateBooksetDto extends PartialType(CreateBooksetDto) {
+       @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsArray()
+  books?: number[];  
+
+  @IsOptional()
+  header: {
+    @IsOptional()
+    @IsNumber()
+    editedBy?: number;
+  };
+}
