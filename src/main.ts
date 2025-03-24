24acbee9d428c2798d0391344ca6068e77da871e
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2025-03-24 14:18:43 +0200

    Revert "CORS troubleshooting test"
    This reverts commit 687b970eaabfd40c1b8e704bc8fda9c1fd0cfb08.

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: '*', // Frontend domain
+    origin: process.env.CLIENT_URL, // Frontend domain
     credentials: true, // Allow cookies
   });
   const swaggerConfig = new DocumentBuilder()
